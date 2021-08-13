340ba756848e6260e18a6e5bb15b423331231111
newseoyun <newseoyun@gmail.com>
2021-08-13 17:19:11 +0900

    init

---
+++ src/test/java/study/seoyun/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package study.seoyun.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
